fb0669f7b50144bcd988862db88c1f1399e925e4
Matthew Harvey <software@matthewharvey.net>
2014-03-15 12:50:03 +1030

    Create class for representing intervals.

--- include/interval.hpp
+++ include/interval.hpp
@@ -1,33 +1,41 @@
-/*
- * Copyright 2014 Matthew Harvey
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2014 Matthew Harvey
 
 #ifndef GUARD_interval_hpp_6955864893717628
 #define GUARD_interval_hpp_6955864893717628
 
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/optional.hpp>
+
 namespace swx
 {
 
+/**
+ * Represent an interval of time. It may be open (still running) or
+ * closed (has an end).
+ */
 class Interval
 {
+// special member functions
 public:
+
+	explicit Interval(boost::posix_time::ptime const& p_begin);
+
 	Interval(Interval const& rhs) = default;
 	Interval(Interval&& rhs) = default;
 	Interval& operator=(Interval const& rhs) = default;
 	Interval& operator=(Interval&& rhs) = default;
 	~Interval() = default;
+
+// ordinary member functions
+public:
+	
+	void set_end(boost::posix_time::ptime const& p_end);
+
+// member variables
+private:
+	boost::posix_time::ptime m_begin;
+	boost::optional<boost::posix_time::ptime> m_end;
+
 
 };  // class Interval
 
--- src/interval.cpp
+++ src/interval.cpp
@@ -1,23 +1,17 @@
-/*
- * Copyright 2014 Matthew Harvey
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2014 Matthew Harvey
 
 #include "interval.hpp"
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <jewel/assert.hpp>
+
+namespace posix_time = boost::posix_time;
 
 namespace swx
 {
 
+Interval::Interval(posix_time::ptime const& p_begin): m_begin(p_begin)
+{
+	JEWEL_ASSERT (!m_end);
+}
 
 }  // namespace swx
