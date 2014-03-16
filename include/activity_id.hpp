570d96ff1980fd9b03f9a491efc96147bb14c46e
Matthew Harvey <software@matthewharvey.net>
2014-03-16 13:29:07 +1030

    Create ActivityId typedef.

---
+++ include/activity_id.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2014 Matthew Harvey
+ */
+
+#ifndef GUARD_activity_id_hpp_6226705269062288
+#define GUARD_activity_id_hpp_6226705269062288
+
+#include <cstddef>
+
+namespace swx
+{
+
+typedef std::size_t ActivityId;
+
+}  // namespace swx
+
+#endif  // GUARD_activity_id_hpp_6226705269062288
