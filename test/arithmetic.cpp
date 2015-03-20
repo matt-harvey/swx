39635f925b085a8b952679133ebb5aac27975a54
Matthew Harvey <software@matthewharvey.net>
2015-03-20 20:25:52 +1030

    Write tests for swx::round.

--- test/arithmetic.cpp
+++ test/arithmetic.cpp
@@ -20,13 +20,13 @@
 
 using std::numeric_limits;
 
-namespace swx
-{
 namespace test
 {
 
 BOOST_AUTO_TEST_CASE(addition_is_safe_test)
 {
+	using swx::addition_is_safe;
+
 	unsigned long long const ullmax = numeric_limits<unsigned long long>::max();
 	
 	// test reaction to safe operations
@@ -42,8 +42,23 @@ BOOST_AUTO_TEST_CASE(addition_is_safe_test)
 
 BOOST_AUTO_TEST_CASE(round)
 {
-	// TODO
+	using swx::round;
+
+	BOOST_CHECK_CLOSE(round(1.0, 1, 1), 1.0, 0.0001);
+	BOOST_CHECK_CLOSE(round(1.0, 1, 2), 1.0, 0.0001);
+	BOOST_CHECK_CLOSE(round(1.0, 2, 1), 2.0, 0.0001);
+	BOOST_CHECK_CLOSE(round(-1.0, 2, 1), -2.0, 0.0001);
+	BOOST_CHECK_CLOSE(round(0.5, 1, 2), 0.5, 0.0001);
+	BOOST_CHECK_CLOSE(round(0.4, 1, 2), 0.5, 0.0001);
+	BOOST_CHECK_CLOSE(round(0.12, 1, 10), 0.1, 0.0001);
+	BOOST_CHECK_CLOSE(round(-0.04, 1, 10), 0.0, 0.0001);
+	BOOST_CHECK_CLOSE(round(-0.24, 1, 10), -0.2, 0.0001);
+	BOOST_CHECK_CLOSE(round(-0.24, 1, 4), -0.25, 0.0001);
+	BOOST_CHECK_CLOSE(round(-600.124, 1, 4), -600.0, 0.0001);
+	BOOST_CHECK_CLOSE(round(-600.5, 1, 4), -600.5, 0.0001);
+	BOOST_CHECK_CLOSE(round(3.64, 1, 4), 3.75, 0.0001);
+	BOOST_CHECK_CLOSE(round(0.8, 2, 5), 0.8, 0.0001);
+	BOOST_CHECK_CLOSE(round(0.9, 2, 5), 0.8, 0.0001);
 }
 
 }  // namespace test
-}  // namespace swx
